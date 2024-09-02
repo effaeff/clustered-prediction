22a40d6fa898724cbf6e6b7221f3abdcae154e97
Felix Finkeldey <felix.finkeldey@tu-dortmund.de>
2024-09-02 15:13:57 +0200

    define problem cases in config

--- config.py
+++ config.py
@@ -26,16 +26,38 @@ N_ITER_SEARCH = 20
 
 VERBOSE = False
 
+PROBLEM_CASES = [
+    16001,
+    20002,
+    21001,
+    29001,
+    30001,
+    31001,
+    36001,
+    38001,
+    40001,
+    41001,
+    44001,
+    48001,
+    49001,
+    50001,
+    58001,
+    60002,
+    61001,
+    62003,
+    64001
+]
+
 PARAM_DICTS = [
-    {
-        'learning_rate': uniform(0.0001, 0.1),
-        'max_depth': randint(2, 32),
-        'subsample': uniform(0.5, 0.5),
-        'n_estimators': randint(100, 1000),
-        'colsample_bytree': uniform(0.4, 0.6),
-        'lambda': randint(1, 100),
-        'gamma': uniform()
-    }
+    # {
+        # 'learning_rate': uniform(0.0001, 0.1),
+        # 'max_depth': randint(2, 32),
+        # 'subsample': uniform(0.5, 0.5),
+        # 'n_estimators': randint(100, 1000),
+        # 'colsample_bytree': uniform(0.4, 0.6),
+        # 'lambda': randint(1, 100),
+        # 'gamma': uniform()
+    # }
     # {
         # 'learning_rate': uniform(0.0001, 0.1),
         # 'n_estimators': randint(100, 1000)
@@ -48,17 +70,17 @@ PARAM_DICTS = [
         # 'min_samples_leaf': randint(2, 11),
         # 'max_features': randint(1, INPUT_SIZE)
     # },
-    # {
-        # 'n_estimators': randint(100, 1000),
-        # 'max_depth': randint(2, 32),
-        # 'min_samples_split': randint(2, 11),
-        # 'min_samples_leaf': randint(2, 11),
-        # 'max_features': randint(1, INPUT_SIZE)
-    # }
+    {
+        'n_estimators': randint(100, 1000),
+        'max_depth': randint(2, 32),
+        'min_samples_split': randint(2, 11),
+        'min_samples_leaf': randint(2, 11),
+        'max_features': randint(1, INPUT_SIZE)
+    }
 ]
 REGRESSORS = [
-    [xgb.XGBRegressor(objective='reg:squarederror') for __ in range(OUTPUT_SIZE)]
+    # [xgb.XGBRegressor(objective='reg:squarederror') for __ in range(OUTPUT_SIZE)]
     # [AdaBoostRegressor(random_state=RANDOM_SEED) for __ in range(OUTPUT_SIZE)],
     # [GradientBoostingRegressor(random_state=RANDOM_SEED) for __ in range(OUTPUT_SIZE)],
-    # [RandomForestRegressor(random_state=RANDOM_SEED, n_jobs=-1) for __ in range(OUTPUT_SIZE)]
+    [RandomForestRegressor(random_state=RANDOM_SEED, n_jobs=-1) for __ in range(OUTPUT_SIZE)]
 ]
